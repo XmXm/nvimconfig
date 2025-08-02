976a7d36a7191645084c8bb69ad2deb79a240ce9
zyx <zyx@Ansz-MP.local>
2025-08-02 14:14:25 +0800

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local map = LazyVim.safe_keymap_set
+local map = vim.keymap.set
 map("n", "<C-z>", "u", { desc = "Undo" })
 map("i", "<C-z>", "<C-o>u", { desc = "Undo" })
 map("n", "<C-y>", "<C-r>", { desc = "Redo" })
