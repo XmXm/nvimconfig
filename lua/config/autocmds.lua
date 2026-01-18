0cdd0cc9a4eb60e7e408c5582f102ed1e4e53282
zyx <zy_s_h@me.com>
2026-01-18 21:25:56 +0800

    update json format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+local set_indent = function(size)
+	vim.opt_local.expandtab = true
+	vim.opt_local.tabstop = size
+	vim.opt_local.shiftwidth = size
+	vim.opt_local.softtabstop = size
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "yaml", "typescript", "javascript" },
+	callback = function()
+		set_indent(2) -- 这些文件类型使用 2 空格
+	end,
+})
